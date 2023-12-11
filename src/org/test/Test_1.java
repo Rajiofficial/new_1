3cee269359fb3fbc633009982a512dc8489c90e5
raji <rajfernanto765@gmail.com>
2023-12-11 21:07:45 +0530

    first commit

---
+++ src/org/test/Test_1.java
@@ -0,0 +1,13 @@
+package org.test;
+
+public class Test_1 {
+	void check() {
+		int d=23;
+		System.out.println(d);
+	}
+public static void main(String[] args) {
+	System.out.println("console");
+	Test_1 out=new Test_1();
+	out.check();
+}
+}
